38713003c72c37d7a1684b2537699efd82281e47
Mayda Gomez <Maygomezatx@Mays-MBP.lan>
2022-05-21 12:40:48 -0500

    Assesment 1 commit

---
+++ Project2/script.js
@@ -0,0 +1,23 @@
+var name = prompt("Tell me you name.");
+var x = Number(prompt("Give me a number"));
+var y = Number(prompt("Give me another number")); {
+  message = (name)
+};
+
+document. write();
+
+function myName() {
+ return name; //Telling the function to return name/what is entered by the user when prompted for name
+};
+
+document.write("You are going to have a wonderful day, " + name + ".");
+
+function add(x, y) {
+  var x = Number(prompt("Give me a number"));
+var y = Number(prompt("Give me another number"));
+return(x + y); //the function will return sum of x + y
+}
+
+document.write(" By the way, the sum of your numbers is " + (x + y) + ".");
+
+document.body.style.fontSize = "96px";
